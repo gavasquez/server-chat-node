e8c515c1648f92e83e2860b85ca1bbba0d88c435
Gustavo Vasquez <gustavo.vasquez@corhuila.edu.co>
2024-03-10 14:22:02 -0500

    Lista de usuarios, conexion

--- models/sockets.js
+++ models/sockets.js
@@ -1,3 +1,5 @@
+const { usuarioConectado, usuarioDesconectado, getUsuarios } = require("../controllers/sockets");
+const { comprobarJWT } = require("../helpers/jwt");
 
 class Sockets {
     constructor(io) {
@@ -7,23 +9,36 @@ class Sockets {
 
     socketEvents() {
         // On connection
-        this.io.on('connection', (socket) => {
-           // TODO: Validar el jwt
-           // Si el token no es valido, desconectarlo
+        this.io.on('connection', async (socket) => {
+            // Identificar persona conectada
+            // TODO: Validar el jwt
+            const [valido, uid] = comprobarJWT(socket.handshake.query['x-token']);
+            // Si el token no es valido, desconectarlo
+            if (!valido) {
+                console.log('Socket no indentificado');
+                return socket.disconnect();
+            }
+            // TODO: Saber que usuario esta activo mediante el UID
+            console.log('Cliente conectado', uid);
+            await usuarioConectado(uid);
 
-           // TODO: Saber que usuario esta activo mediante el UID
+            // TODO: Emitir todos los usuarios conectados
+            this.io.emit('lista-usuarios', await getUsuarios());
+            // TODO: Socket join, uid
 
-           // TODO: Emitir todos los usuarios conectados
+            // TODO: Escuchar cuando el cliente envia un mensaje
+            //* mensaje-personal
 
-           // TODO: Socket join, uid
+            // TODO: Disconnect
+            // Marcar en la bd que el usuario se desconecto
+            socket.on('disconnect', async () => {
+                console.log('Cliente desconectado', uid)
+                usuarioDesconectado(uid);
+                // Cuando se desconecta un usuario enviamos nuevamente la lista de los usuarios conectados
+                this.io.emit('lista-usuarios', await getUsuarios());
+            });
 
-           // TODO: Escuchar cuando el cliente envia un mensaje
-           //* mensaje-personal
-
-           // TODO: Disconnect
-           // Marcar en la bd que el usuario se desconecto
-
-           // TODO: Emitir todos los usuarios conectados
+            // TODO: Emitir todos los usuarios conectados
         });
     }
 }
